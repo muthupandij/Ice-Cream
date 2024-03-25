514c07aba2738529b1443aaa8c0d925cf4ef11a4
Muthupandi <153271612+muthupandij@users.noreply.github.com>
2024-03-25 17:38:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function clicked(){
+    let a=document.getElementById("icon-bar")
+    if(a.style.display === "none"){
+        a.style.display="block";
+    }else{
+        a.style.display="none";
+    }
+};
